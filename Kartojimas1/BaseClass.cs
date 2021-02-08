3809a6444a9074f66e5925c3af23b75a5b5bdd60
Modestas Paulauskas <modispaulauskas@gmail.com>
2021-02-08 20:25:43 +0200

    Add project files.

---
+++ Kartojimas1/BaseClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kartojimas1
+{
+   public abstract class BaseClass
+    {
+        public string Name { get; }
+
+        protected BaseClass(string name)
+        {
+            Name = name;
+        }
+
+        public string NameMetodas()
+        {
+            return "";
+        }
+
+        public int NameKodas(int pirmasKint)
+        {
+            return pirmasKint;
+        }
+    }
+}
